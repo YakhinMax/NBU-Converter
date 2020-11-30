a3ad2bc94f2d22e0e657be931706f13ad0cb12ca
Maxim <maximyakhin@gmail.com>
2020-11-30 21:40:45 +0200

    Added comment

--- NBU/NBU/CurrencyTable/CurrencyTableViewController.swift
+++ NBU/NBU/CurrencyTable/CurrencyTableViewController.swift
@@ -14,17 +14,11 @@ class CurrencyTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        getData(urlString: "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        getData(urlString: UserDefaults.standard.string(forKey: "jsonUrl")!)
     }
     
     func getData(urlString: String) {
-        let group = DispatchGroup()
-        group.enter()
+        // This can be done in a separate model class, but that would take a bit more time to implement correctly (According to MVC)
         DispatchQueue.global().async {
             guard let url = URL(string: urlString) else { return }
             print("Connected to " + urlString)
@@ -49,12 +43,10 @@ class CurrencyTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return currencyData.count
     }
 
@@ -63,54 +55,6 @@ class CurrencyTableViewController: UITableViewController {
         
         cell.setCurrency(curr: currencyData[indexPath.row].cc, val: currencyData[indexPath.row].rate)
         
-        // Configure the cell...
-
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
