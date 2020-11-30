aa8b95b2a9fa7cc3504748cac654da47f7c5b55b
Maxim <maximyakhin@gmail.com>
2020-11-30 16:59:46 +0200

    Initial screens setup

---
+++ NBU/NBU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NBU
+//
+//  Created by Maxim Yakhin on 30.11.2020.
+//  Copyright © 2020 Max Yakhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
