3d63e7d6e56476b06df511fba8142bfd05305422
Maxim <maximyakhin@gmail.com>
2020-11-30 21:40:32 +0200

    Extension for the DataSource protocol

---
+++ NBU/NBU/ExchangeController/DataSourceExtension.swift
@@ -0,0 +1,19 @@
+//
+//  DataSourceExtension.swift
+//  NBU
+//
+//  Created by Maxim Yakhin on 30.11.2020.
+//  Copyright © 2020 Max Yakhin. All rights reserved.
+//
+
+import UIKit
+
+extension ExchangeViewController : UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyData.count
+    }
+}
